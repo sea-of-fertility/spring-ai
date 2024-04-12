9c05bce90b201570821cf0f96814e463dbb17e9a
LetgoMoveNotLate <hj981203@gmail.com>
2024-04-12 16:16:14 +0900

    FIX: gradle open AI dependencies 삭제

--- src/main/java/com/example/springai/controller/ChatController.java
+++ src/main/java/com/example/springai/controller/ChatController.java
@@ -1,2 +1,42 @@
-package com.example.springai.controller;public class OllamaChatClient {
+package com.example.springai.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@RestController
+@Slf4j
+public class ChatController {
+
+    private final OllamaChatClient chatClient;
+
+    @Autowired
+    public ChatController(OllamaChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        String call = chatClient.call(message);
+        
+        log.info("{}",call);
+        return Map.of("generation", call);
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatClient.stream(prompt);
+    }
+
+
 }
